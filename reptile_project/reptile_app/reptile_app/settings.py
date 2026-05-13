7cca384e06f25825b724e3372333d7248228d35a
toyoda0 <yusiryta@gmail.com>
2026-05-13 19:21:39 +0900

    ユーザークラスのカスタマイズ

--- reptile_project/reptile_app/reptile_app/settings.py
+++ reptile_project/reptile_app/reptile_app/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'user',
 ]
 
+AUTH_USER_MODEL = 'user.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- reptile_project/reptile_app/user/models.py
+++ reptile_project/reptile_app/user/models.py
@@ -1,6 +1,45 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import(
+    BaseUserManager, AbstractBaseUser, PermissionsMixin
+)
 import uuid
+
+class UserManager(BaseUserManager):
+    
+    def create_user(self, email, username, password=None, **extra_fields):
+        if not email:
+            raise ValueError('メールアドレスは必須です')
+        if not username:
+            raise ValueError('ユーザー名は必須です')
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        extra_fields['is_staff'] = True
+        extra_fields['is_active'] = True
+        extra_fields['is_superuser'] = True
+        return self.create_user(email, username, password, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    
+    username = models.CharField(max_length=150)
+    email = models.EmailField(max_length=255, unique=True)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    website = models.URLField(null=True)
+    picture = models.FileField(null=True)
+    
+    objects = UserManager()
+    
+    USERNAME_FIELD = 'email' #このテーブルのレコードを一意に識別するField
+    REQUIRED_FIELDS = ['username'] #createsuperuserの時に入力を求められる
+    
+    def __str__(self):
+        return self.email
+
 
 #パスワード再発行
 class PasswordResetToken(models.Model):
