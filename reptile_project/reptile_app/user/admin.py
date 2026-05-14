9811f34d5348c123edc737a8aba9e791647772f6
toyoda0 <yusiryta@gmail.com>
2026-05-14 20:25:16 +0900

    Custom Userを実装

--- reptile_project/reptile_app/user/admin.py
+++ reptile_project/reptile_app/user/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from .forms import UserCreationForm, UserChangeForm
 
-# Register your models here.
+
+User = get_user_model()
+
+class CustomizeUserAdmin(UserAdmin):
+    form = UserChangeForm #編集画面
+    add_form = UserCreationForm #ユーザー作成画面
+    #一覧で表示するフィールド
+    list_display = ('username', 'email', 'is_staff')
+    
+    fieldsets = (
+        ('ユーザー情報', {
+                'fields': ('username', 'email', 'password', 'website', 'picture')
+            }),
+        ('パーミッション', {'fields': ('is_staff', 'is_active', 'is_superuser')})
+    )
+    
+    add_fieldsets = (
+        (
+            'ユーザー情報',{
+                'fields':('username', 'email', 'password1', 'password2')
+            }
+            
+        ),
+    )
+
+admin.site.register(User, CustomizeUserAdmin)
--- reptile_project/reptile_app/user/forms.py
+++ reptile_project/reptile_app/user/forms.py
@@ -1,10 +1,46 @@
 from django import forms
-from django.contrib.auth.models import User
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
+
+User = get_user_model()
+
+#ユーザー作成用のフォーム    
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='パスワード', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='パスワード(確認)', widget=forms.PasswordInput)
+    
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+        
+    def clean(self):
+        cleaned_data = super().clean()
+        password1 = cleaned_data.get('password1')
+        password2 = cleaned_data.get('password2')
+        if password1 != password2:
+            raise ValidationError('パスワードが一致しません')
+        
+    def save(self, commit=False):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data.get('password1'))
+        user.save()
+        return user
+
+#ユーザー編集用のフォーム    
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField(label='パスワード')
+    
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'is_active', 'is_staff', 'is_superuser')
+        
+    def clean_password(self):
+        return self.instance.password
 
 class UserForm(forms.ModelForm):
     
-    class Meta():
+    class Meta:
         model = User
         fields = ('username', 'email', 'password')
         widgets = {
