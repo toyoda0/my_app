466002980f37e65bc0ced15476e676008499d382
toyoda0 <yusiryta@gmail.com>
2026-07-07 21:28:43 +0900

    fix: update ALLOWED_HOSTS for real domain

--- reptile_project/reptile_app/reptile_app/settings.py
+++ reptile_project/reptile_app/reptile_app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-@hn&7-yuyj+ypuf+%*b)mdh^xj6rhp1dm2ce*-2%wglv!crvj-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'toyoda.pythonanywhere.com']
 
 
 # Application definition
